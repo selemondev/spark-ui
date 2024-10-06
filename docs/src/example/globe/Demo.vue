1d2fb6c0eafe0ecd2c86e474d9a0063784700002
selemondev <selemondev19@gmail.com>
2024-10-06 22:58:58 +0300

    feat: add globe component

---
+++ docs/src/example/globe/Demo.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+import Globe from './Globe.vue'
+</script>
+
+<template>
+  <div>
+    <div
+      class="relative flex h-[400px] w-[500px] max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border px-40 pb-40 pt-8 md:pb-48"
+    >
+      <span
+        class="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
+      >
+        Globe
+      </span>
+      <Globe />
+      <div
+        class="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]"
+      />
+    </div>
+  </div>
+</template>
---
+++ docs/src/example/globe/Globe.vue
@@ -0,0 +1,119 @@
+<script setup lang="ts">
+import createGlobe from 'cobe'
+import { onMounted, ref } from 'vue'
+import { useSpring } from 'vue-use-spring'
+
+const canvasRef = ref<HTMLCanvasElement | null>(null)
+const pointerInteracting = ref<number | null>(null)
+const pointerInteractionMovement = ref(0)
+const phi = ref(0)
+
+const api = useSpring(
+  { r: 0 },
+  {
+    mass: 1,
+    tension: 280,
+    friction: 40,
+    precision: 0.001,
+  },
+)
+
+onMounted(() => {
+  createGlobe(canvasRef.value!, {
+    devicePixelRatio: 2,
+    width: 1000,
+    height: 1000,
+    phi: 0,
+    theta: 0,
+    dark: 1,
+    diffuse: 1.2,
+    mapSamples: 16000,
+    mapBrightness: 6,
+    baseColor: [0.3, 0.3, 0.3],
+    markerColor: [0.1, 0.8, 1],
+    glowColor: [1, 1, 1],
+    markers: [
+      // longitude latitude
+      { location: [37.7595, -122.4367], size: 0.03 },
+      { location: [40.7128, -74.006], size: 0.1 },
+    ],
+    onRender: (state) => {
+      // This prevents rotation while dragging
+      if (!pointerInteracting.value) {
+        // Called on every animation frame.
+        // `state` will be an empty object, return updated params.
+        phi.value += 0.005
+      }
+      state.phi = phi.value + api.r
+    },
+  })
+  canvasRef.value!.style.opacity = '1'
+})
+
+function handlePointerDown(e: PointerEvent) {
+  pointerInteracting.value = e.clientX - pointerInteractionMovement.value
+  canvasRef.value!.style.cursor = 'grabbing'
+}
+
+function handlePointerUp(_: PointerEvent) {
+  pointerInteracting.value = null
+  canvasRef.value!.style.cursor = 'grab'
+}
+
+function handlePointerOut(_: PointerEvent) {
+  pointerInteracting.value = null
+  canvasRef.value!.style.cursor = 'grab'
+}
+
+function handleMouseMove(e: MouseEvent) {
+  if (pointerInteracting.value !== null) {
+    const delta = e.clientX - pointerInteracting.value
+    pointerInteractionMovement.value = delta
+    api.r = delta / 200
+  }
+}
+
+function handleTouchMove(e: TouchEvent) {
+  if (pointerInteracting.value !== null && e.touches[0]) {
+    const delta = e.touches[0].clientX - pointerInteracting.value
+    pointerInteractionMovement.value = delta
+    api.r = delta / 100
+  }
+}
+</script>
+
+<template>
+  <div class="absolute inset-0 mx-auto aspect-[1/1] w-full max-w-[600px] top-44">
+    <canvas
+      ref="canvasRef"
+      class="w-[500px] h-[500px] cursor-grab opacity-0 transition-opacity duration-500 [contain:layout_paint_size]"
+      width="1000"
+      height="1000"
+      @pointerdown="handlePointerDown"
+      @pointerup="handlePointerUp"
+      @pointerout="handlePointerOut"
+      @mousemove="handleMouseMove"
+      @touchmove="handleTouchMove"
+    />
+  </div>
+</template>
+
+<style>
+.app {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.globe {
+  width: 500px;
+  height: 500px;
+  cursor: grab;
+  contain: layout paint size;
+  opacity: 0;
+  transition: opacity 1s ease;
+}
+</style>
