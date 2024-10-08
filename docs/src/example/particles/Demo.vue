1b1dec30d6f79739ff2bf36c434b4ad7b2ec45ab
selemondev <selemondev19@gmail.com>
2024-10-08 12:24:23 +0300

    feat: add particles component

---
+++ docs/src/example/particles/Demo.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+import HeroParticles from './Particles.vue'
+import HeroBackground from './ParticlesBackground.vue'
+</script>
+
+<template>
+  <div class="relative w-[600px] h-[400px]">
+    <HeroParticles :accelerate="true" class="absolute top-0" />
+    <HeroBackground />
+  </div>
+</template>
