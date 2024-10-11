e8924d730c4217686c9dd45e2defdd93d2e55733
selemondev <selemondev19@gmail.com>
2024-10-11 23:41:51 +0300

    doc: add orbiting circles

---
+++ docs/src/components/icons/Nitro.vue
@@ -0,0 +1,70 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+    <g fill="none">
+      <g clip-path="url(#unjsNitro4)">
+        <path
+          fill="url(#unjsNitro1)" fill-rule="evenodd"
+          d="M28.173 5.616C22.438-1.107 12.34-1.907 5.617 3.828s-7.524 15.834-1.789 22.557s15.834 7.523 22.557 1.788s7.523-15.834 1.788-22.557m-7.97 8.398c.616 0 1.01.668.7 1.202l-.089.153l-6.038 9.935a.62.62 0 0 1-.529.297h-.576a.604.604 0 0 1-.585-.755l1.638-6.335a.8.8 0 0 0-.774-1h-2.517a.8.8 0 0 1-.774-1l2.472-9.565a.74.74 0 0 1 .716-.544q.061 0 .107.002h2.535a.8.8 0 0 1 .793.908l-.016.114l-.966 5.653a.8.8 0 0 0 .788.935z"
+          clip-rule="evenodd"
+        />
+        <path
+          fill="url(#unjsNitro2)" fill-rule="evenodd"
+          d="M28.173 5.616C22.438-1.107 12.34-1.907 5.617 3.828s-7.524 15.834-1.789 22.557s15.834 7.523 22.557 1.788s7.523-15.834 1.788-22.557m-7.97 8.398c.616 0 1.01.668.7 1.202l-.089.153l-6.038 9.935a.62.62 0 0 1-.529.297h-.576a.604.604 0 0 1-.585-.755l1.638-6.335a.8.8 0 0 0-.774-1h-2.517a.8.8 0 0 1-.774-1l2.472-9.565a.74.74 0 0 1 .716-.544q.061 0 .107.002h2.535a.8.8 0 0 1 .793.908l-.016.114l-.966 5.653a.8.8 0 0 0 .788.935z"
+          clip-rule="evenodd"
+        />
+        <mask
+          id="unjsNitro0" width="32" height="33" x="0" y="0" maskUnits="userSpaceOnUse"
+          style="mask-type:alpha"
+        >
+          <path
+            fill="url(#unjsNitro3)"
+            d="M16 32.001c8.837 0 16-7.163 16-16s-7.163-16-16-16s-16 7.163-16 16s7.163 16 16 16"
+          />
+        </mask>
+        <g filter="url(#unjsNitro5)" mask="url(#unjsNitro0)">
+          <path
+            fill="#fff"
+            d="M.89 10.741a16 16 0 1 0 30.365.434l-4.652 1.471a11.12 11.12 0 1 1-21.106-.3z"
+          />
+        </g>
+      </g>
+      <defs>
+        <radialGradient
+          id="unjsNitro1" cx="0" cy="0" r="1"
+          gradientTransform="matrix(31.2006 0 0 318.168 3.2 16)" gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#31B2F3" />
+          <stop offset=".474" stop-color="#F27CEC" />
+          <stop offset="1" stop-color="#FD6641" />
+        </radialGradient>
+        <radialGradient
+          id="unjsNitro2" cx="0" cy="0" r="1"
+          gradientTransform="matrix(31.2006 0 0 318.168 3.2 16)" gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#31B2F3" />
+          <stop offset=".474" stop-color="#F27CEC" />
+          <stop offset="1" stop-color="#FD6641" />
+        </radialGradient>
+        <radialGradient
+          id="unjsNitro3" cx="0" cy="0" r="1"
+          gradientTransform="matrix(31.2 0 0 318.162 3.2 16.001)" gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#F27CEC" />
+          <stop offset=".484" stop-color="#31B2F3" />
+          <stop offset="1" stop-color="#7D7573" />
+        </radialGradient>
+        <clipPath id="unjsNitro4">
+          <path fill="#fff" d="M0 0h32v32H0z" />
+        </clipPath>
+        <filter
+          id="unjsNitro5" width="52" height="41.26" x="-10" y=".741" color-interpolation-filters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur result="effect1_foregroundBlur_7_821" stdDeviation="5" />
+        </filter>
+      </defs>
+    </g>
+  </svg>
+</template>
