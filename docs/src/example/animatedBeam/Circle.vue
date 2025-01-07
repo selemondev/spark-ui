abae10ba478addb2d9b61470f5c3e96c9603477f
selemondev <selemondev19@gmail.com>
2025-01-07 23:52:23 +0300

    fix: animated-beam prop types

--- docs/src/example/animatedBeam/Circle.vue
+++ docs/src/example/animatedBeam/Circle.vue
@@ -1,7 +1,7 @@
 <script setup lang='ts'>
 import { ref } from 'vue'
 
-const circleRef = ref()
+const circleRef = ref<HTMLElement>()
 
 defineExpose({
   circleRef,
--- docs/src/example/animatedBeam/Demo.vue
+++ docs/src/example/animatedBeam/Demo.vue
@@ -3,14 +3,14 @@ import { ref } from 'vue'
 import AnimatedBeam from './AnimatedBeam.vue'
 import Circle from './Circle.vue'
 
-const containerRef = ref()
-const div1Ref = ref()
-const div2Ref = ref()
-const div3Ref = ref()
-const div4Ref = ref()
-const div5Ref = ref()
-const div6Ref = ref()
-const div7Ref = ref()
+const containerRef = ref<HTMLElement>()
+const div1Ref = ref<HTMLElement>()
+const div2Ref = ref<HTMLElement>()
+const div3Ref = ref<HTMLElement>()
+const div4Ref = ref<HTMLElement>()
+const div5Ref = ref<HTMLElement>()
+const div6Ref = ref<HTMLElement>()
+const div7Ref = ref<HTMLElement>()
 </script>
 
 <template>
