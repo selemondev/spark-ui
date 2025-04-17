317a61b777114cd4ad887c4a36231e1488d6678e
selemondev <selemondev19@gmail.com>
2025-04-16 21:52:38 -0600

    feat: add terminal

---
+++ docs/src/example/terminal/AnimatedSpan.vue
@@ -0,0 +1,27 @@
+<script setup lang='ts'>
+import type { MotionProps } from 'motion-v'
+import { motion } from 'motion-v'
+import { cn } from '../../lib/utils'
+
+interface AnimatedSpanProps extends MotionProps {
+  delay?: number
+  className?: string
+};
+const props = withDefaults(defineProps<AnimatedSpanProps>(), {
+  delay: 0,
+})
+</script>
+
+<template>
+  <motion.div
+    :initial="{ opacity: 0, y: -5 }" :animate="{
+      opacity: 1, y: 0,
+    }" :transition="{
+      duration: 0.3, delay: props.delay / 1000,
+    }" :class="cn(
+      'grid text-sm font-normal tracking-tight', props.className,
+    )"
+  >
+    <slot />
+  </motion.div>
+</template>
