32625856fdbcc4d67ec478273904c77db2d08503
selemondev <selemondev19@gmail.com>
2026-07-04 03:34:34 -0600

    feat: add Comic Text

---
+++ docs/src/spark-ui-demos/comic-text/comic-text.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ComicText from "../../components/spark-ui/comic-text/comic-text.vue";
+</script>
+
+<template>
+  <div class="space-y-8 text-center">
+    <ComicText :font-size="5">BOOM!</ComicText>
+  </div>
+</template>
