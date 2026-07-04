882f3d8b20a855e31fc807576c9b1dbd986122ee
selemondev <selemondev19@gmail.com>
2026-07-04 10:07:52 -0600

    fix: render Flickering Grid canvas

--- docs/src/spark-ui-demos/flickering-grid/rounded-demo.vue
+++ docs/src/spark-ui-demos/flickering-grid/rounded-demo.vue
@@ -4,7 +4,7 @@ import FlickeringGrid from "../../components/spark-ui/flickering-grid/flickering
 
 <template>
   <div
-    class="relative size-[600px] w-full overflow-hidden rounded-lg border bg-background"
+    class="relative size-[600px] max-w-full overflow-hidden rounded-lg border bg-background"
   >
     <FlickeringGrid
       class="relative inset-0 z-0 [mask-image:radial-gradient(450px_circle_at_center,white,transparent)]"
