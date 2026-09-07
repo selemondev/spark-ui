e5123ccd7ffc66e166128a1b04ee7a319f3639ab
selemondev <selemondev19@gmail.com>
2026-09-06 19:59:16 -0600

    chore(demos): use a scenic wallpaper for android and iphone previews

--- docs/src/example/android/image-demo.vue
+++ docs/src/example/android/image-demo.vue
@@ -6,7 +6,7 @@ import Android from "./android.vue";
   <div class="relative mx-auto flex h-[420px] w-full items-center justify-center">
     <Android
       class="h-full w-auto"
-      src="https://images.unsplash.com/photo-1730326405863-c6fa7e499a6e?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?auto=format&fit=crop&w=900&q=85"
     />
   </div>
 </template>
--- docs/src/spark-ui-demos/iphone/image-iphone.vue
+++ docs/src/spark-ui-demos/iphone/image-iphone.vue
@@ -6,7 +6,7 @@ import Iphone from "../../components/spark-ui/iphone/iphone.vue";
   <div class="relative mx-auto flex h-[440px] w-full items-center justify-center">
     <Iphone
       class="h-full w-auto"
-      src="https://images.unsplash.com/photo-1730326405863-c6fa7e499a6e?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?auto=format&fit=crop&w=900&q=85"
     />
   </div>
 </template>
